dba5444af06da9765e63ddb0206cfbad063d7d4b
paololais <paolo.lais1@gmail.com>
2025-02-24 16:09:11 +0100

    third point

--- main.c
+++ main.c
@@ -11,6 +11,8 @@
 int main(void) {
     ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000;
     
+    bool accesa = false;
+    
     // set the pin as input
     TRISEbits.TRISE8 = 1;
     // set the pin as output
@@ -18,6 +20,7 @@ int main(void) {
     LATAbits.LATA0 = 0;
 
     while(1){
+        /*
         // read button T2 RE8
         if(PORTEbits.RE8 == 0){ 
         // switch on led1 RA0
@@ -25,6 +28,14 @@ int main(void) {
         } else{
             LATAbits.LATA0 = 0;
         }     
+        */
+        if(PORTEbits.RE8 == 0){
+            if(accesa){
+                LATAbits.LATA0 = 0;
+            } else {
+                LATAbits.LATA0 = 1;
+            }
+        }
     }
     
     return 0;
